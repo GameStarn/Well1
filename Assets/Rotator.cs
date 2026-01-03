1217287cc674dcac551a28e2dd2f689fd23400cd
DangDark <semenodnolko@gmail.com>
2026-01-03 18:28:06 +0300

    Трансформации

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cube1 : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
     [SerializeField] private Transform _rotate;
     [SerializeField] private float _speed = 1;
 
     private void Update()
     {
-        _rotate.Rotate(0, _speed, 0);
+        _rotate.Rotate(0, _speed * Time.deltaTime, 0);
     }
 }
--- Assets/Scale.cs
+++ Assets/Scale.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Capsule : MonoBehaviour
+public class Scale : MonoBehaviour
 {
     [SerializeField] private Vector3 _scale = new Vector3(0.1f, 0.1f, 0.1f);
     [SerializeField] private float _speed = 1f;
