0cebdf844a414931c7726c0f8368ff15e2d90452
DangDark <semenodnolko@gmail.com>
2026-01-15 18:18:47 +0300

    Счетчик

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -11,15 +11,44 @@ public class Clock : MonoBehaviour
     private int _clickCount = 0;
     private bool _isPaused = false;
 
-    private void Start()
+    private float _updateCoroutine = 0.5f;
+    private float _delayTime = 10f;
+    private int _increaseCount = 10;
+    private int _numberOfClicks = 2;
+
+    private void OnEnable()
     {
-        StartCoroutine(CounterRoutime(0.5f));
+        StartCoroutine(CounterRoutime(_updateCoroutine));
     }
 
-    private IEnumerator CounterRoutime(float dely)
+    private void OnDisable()
     {
+        StopAllCoroutines();
+    }
 
-        while (true)
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            _count += _increaseCount;
+            _clickCount++;
+
+            if (_clickCount == _numberOfClicks)
+            {
+                _isPaused = true;
+                _clickCount = 0;
+            }
+
+            _text.text = _count.ToString("");
+        }
+    }
+
+    private IEnumerator CounterRoutime(float delay)
+    {
+        WaitForSeconds normalDelay = new WaitForSeconds(delay);
+        WaitForSeconds pauseDelay = new WaitForSeconds(_delayTime);
+
+        while (enabled)
         {
             _text.text = _count.ToString("");
             _count++;
@@ -27,28 +56,13 @@ public class Clock : MonoBehaviour
 
             if (_isPaused)
             {
-                yield return new WaitForSeconds(10f);
+                yield return pauseDelay;
                 _isPaused = false;
             }
             else
             {
-                yield return new WaitForSeconds(dely);
+                yield return normalDelay;
             }
         }
-
-    }
-
-    public void OnButtonClick()
-    {
-        _count += 10;
-        _clickCount++;
-
-        if (_clickCount == 2)
-        {
-            _isPaused = true;
-            _clickCount = 0;
-        }
-
-        _text.text = _count.ToString("");
     }
 }
